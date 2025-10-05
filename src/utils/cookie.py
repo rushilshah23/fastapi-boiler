0c9466fa0729cf89b65aeefe1416f508d68d9afb
rushil <rushilshah23@users.noreply.github.com>
2025-10-05 23:24:54 +0530

    Added max age in set cookie util

--- src/utils/cookie.py
+++ src/utils/cookie.py
@@ -2,11 +2,12 @@ from fastapi import Response
 from common.configs.env import CommonConfig
 
 
-def set_cookie(key:str, value:str, response:Response):
+def set_cookie(key:str, value:str,max_age:int, response:Response):
     response.set_cookie(
     key=key,
     value=value,
     httponly=CommonConfig.COOKIE_HTTP_ONLY,
     samesite=CommonConfig.COOKIE_SAMESITE,  # Lax is okay too, Strict is safest
-    secure=CommonConfig.COOKIE_SECURE        # HTTPS only
+    secure=CommonConfig.COOKIE_SECURE,        # HTTPS only
+    max_age=max_age
 )
