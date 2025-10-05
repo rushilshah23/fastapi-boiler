60307d4684bbd4b1c3d45d26999c6c700d3ec425
rushil <rushilshah23@users.noreply.github.com>
2025-10-05 20:52:10 +0530

    Added test.env for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,14 @@ import pytest
 import warnings
 from fastapi.testclient import TestClient
 from src.app import create_app
-
+from dotenv import load_dotenv
+from pathlib import Path
 # Suppress Starlette multipart deprecation warnings
 warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / "envs" / ".env")         # default
+load_dotenv(BASE_DIR / "envs" / "test.env", override=True)  # override with test env
 
 @pytest.fixture(scope="module")
 def test_app():
